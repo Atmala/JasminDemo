0c3be1280f095090e185ead3656385e247f0bdb0
Kate Astafieva <kastafieva@dio-soft.com>
2014-08-14 02:03:48 +0300

    One method to show

---
+++ Jasmine/Scripts/targetModule.js
@@ -0,0 +1,11 @@
+﻿var TM = (function () {
+    var Target = function() {};
+
+    Target.prototype = {
+        doTestableAction: function(domId) {
+            $('#' + domId).val('verifyText');
+        }
+    };
+
+    return Target;
+}());
---
+++ Jasmine/Scripts/targetModuleTest.js
@@ -0,0 +1,10 @@
+﻿describe("Target module", function () {
+    var module = new TM();
+
+    it("can setup text to given control", function () {
+        module.doTestableAction('inpTarget');
+        var value = $("#inpTarget").val();
+        expect(value === 'verifyText').toBe(true);
+    });
+
+});
